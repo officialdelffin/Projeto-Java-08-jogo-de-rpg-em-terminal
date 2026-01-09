a8739cfa514feac469fb9c45de0099a681b1f655
officialdelffin <workdelffin@gmail.com>
2026-01-09 20:11:44 -0300

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/* Projeto : Jogo de RPG em terminal
+
+Esse projeto foi desenvolvido para consolidar conceitos do curso de Java com Orientação a Objetos.
+O projeto se trata de um jogo de RPG feito no terminal onde o usuário joga através do terminal. Nesse Jogo é possível o usuário criar o seu persongem e nele vai ter um sistema de RPG de turno, onde é possível
+efetuar ataque, se curar e envenenar o adiversário. */
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
