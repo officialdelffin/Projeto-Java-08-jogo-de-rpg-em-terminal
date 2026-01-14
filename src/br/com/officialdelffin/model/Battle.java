81c0b37a51edd0e0ad7a7f6acd912b038ffbc821
officialdelffin <workdelffin@gmail.com>
2026-01-13 21:22:25 -0300

    commit 16

--- src/br/com/officialdelffin/model/Battle.java
+++ src/br/com/officialdelffin/model/Battle.java
@@ -7,15 +7,17 @@ public class Battle {
     // Atributos :
 
     // Atributos do personagem :
-    int vidaPersonagem   = 0;
-    int danoPersonagem   = 0;
-    int defesaPersonagem = 0;
+    String nomePersonagem  = "";
+    int vidaPersonagem     = 0;
+    int danoPersonagem     = 0;
+    int defesaPersonagem   = 0;
 
 
     // Atributos do rival :
-    int vidaRival   = 0;
-    int danoRival   = 0;
-    int defesaRival = 0;
+    String nomeRival  = "";
+    int vidaRival     = 0;
+    int danoRival     = 0;
+    int defesaRival   = 0;
 
 
     // Métodos :
@@ -25,12 +27,14 @@ public class Battle {
 
 
             // Parametros do personagem :
+            String nomePersonagemP,
             int vidaPersonagemP,
             int danoPersonagemP,
             int defesaPersonagemP,
 
 
             // Parametros do rival :
+            String nomeRivalP,
             int vidaRivalP,
             int danoRivalP,
             int defesaRivalP
@@ -40,9 +44,11 @@ public class Battle {
 
 
         // Atribuindo parametros com as variáveis :
+        nomePersonagem    = nomePersonagemP;
         vidaPersonagem    = vidaPersonagemP;
         danoPersonagem    = danoPersonagemP;
         defesaPersonagem  = defesaPersonagemP;
+        nomeRival         = nomeRivalP;
         vidaRival         = vidaRivalP;
         danoRival         =danoRivalP;
         defesaRival       = defesaRivalP;
--- src/br/com/officialdelffin/model/Enemy.java
+++ src/br/com/officialdelffin/model/Enemy.java
@@ -5,13 +5,78 @@ public class Enemy {
 
 
     // Atributos :
-    String nome  = "";
-    int vida     = 0;
-    int dano     = 0;
-    int defesa   = 0;
+
+    // Dados do rival :
+    private String nome  = "";
+    private int vida     = 0;
+    private int dano     = 0;
+    private int defesa   = 0;
 
 
-    // Metodos :
+    // Métodos :
+
+    // Cria o rival com base nos dados informados :
+    public void criaRival (
+
+
+            // Parametros do rival
+            String nomeP,
+            int vidaP,
+            int danoP,
+            int defesaP
+
+
+    ) {
+
+
+        // Armazenando parametros dentro dos atributos do rival :
+        nome = nomeP;
+        vida = vidaP;
+        dano = danoP;
+        defesa = defesaP;
+
+
+
+    }
+
+
+    // Métodos que exibe dados do rival :
+
+    // Exibir nome do personagem :
+    public String getNome () {
+
+
+        return nome;
+
+    }
+
+
+    // Exibir vida do personagem :
+    public int getVida () {
+
+
+        return vida;
+
+    }
+
+
+    // Exibir dano do personagem :
+    public int getDano () {
+
+
+        return dano;
+
+    }
+
+
+    // Exibir defesa do personagem :
+    public int getDefesa () {
+
+
+        return defesa;
+
+    }
+
 
 
 }
